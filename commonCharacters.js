34626e430ff14f94a23551750849169b1bf49c6a
iheb <117437699+ihebKedri@users.noreply.github.com>
2024-06-05 08:52:49 +0100

    commonCharacters.js
    commonCharacters

---
+++ commonCharacters.js
@@ -0,0 +1,21 @@
+/**
+
+ *
+
+ * Write a function `f(a, b)` which takes two strings as arguments and returns a
+
+ * string containing the characters found in both strings (without duplication), in the
+
+ * order that they appeared in `a`. Remember to skip spaces and characters you
+
+ * have already encountered!
+
+ *
+
+ * Example: commonCharacters('acexivou', 'aegihobu')
+
+ * Returns: 'aeiou'
+
+ * Should return null or empty string of there is no commonCharacters
+
+ *
